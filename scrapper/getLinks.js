fe3e62447ad65aede0c2b8441ccf8dc0802468cb
Omansh Sharma <74657563+arduR-O@users.noreply.github.com>
2025-01-17 15:09:22 +0530

    feat: getLinks function

---
+++ scrapper/getLinks.js
@@ -0,0 +1,44 @@
+import { writeFileSync } from "fs";
+import { By, Builder, Browser } from "selenium-webdriver";
+
+function sleep(ms) {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, ms)
+  );
+}
+export async function getLinks(link, fileName) {
+  const driver = await new Builder().forBrowser(Browser.CHROME).build();
+  try {
+    await driver.get(link);
+    await sleep(5000).then(() => console.log("Iran"));
+    const selector = `//*[@id="main_content"]/div/div/section/div/div/div[5]/div[2]/div[1]/div[2]/ul`;
+
+    const xpaths = Array(50)
+      .fill(selector)
+      .map((e, i) => e + `/li[${i + 1}]/a`);
+    console.log(xpaths);
+    const links = {};
+    for (const [i, path] of xpaths.entries()) {
+      try {
+        const webEle = await driver.findElement(By.xpath(path));
+        const link = await webEle.getAttribute("href");
+        links[i] = link;
+        const jsonLinks = JSON.stringify(links, null, 2);
+        writeFileSync(`${fileName}.json`, jsonLinks);
+      } catch(error) {
+        console.log("Hey Bhagwaan ", error);
+        break;
+      }
+    }
+    driver.quit();
+    return links;
+  } catch (err) {
+    driver.quit();
+    console.log("Hey Raaam!!  ", err);
+  }
+}
+const link = "https://www.olx.in/en-in/cars_c84";
+const fileName = "carLinks";
+getLinks(link, fileName).then((e) => console.log(e));
---
+++ scrapper/sample.js
@@ -0,0 +1,3 @@
+for(i of Array(100).keys()){
+    console.log(i);
+}
